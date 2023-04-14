d7745e66f84d2f04971729959272a6213e744701
sern <sern2001@hotmail.com>
2023-04-14 21:15:38 +0700

    Add Authentication System

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 
 const connectDB = require("./config/db");
 //load env vars
@@ -8,8 +9,18 @@ dotenv.config({ path: "./config/config.env" });
 //connect to database
 connectDB();
 
+//Route files
+const auth = require(`./routes/auth`);
+
 const app = express();
 
+// body parser
+app.use(express.json());
+
+//cookie parser
+app.use(cookieParser());
+
+app.use("/api/v1/auth", auth);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
